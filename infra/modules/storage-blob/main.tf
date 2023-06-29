a2f93775601b66aebc9daedbd50c7649e960d0cd
April Edwards <scubaninja@github.com>
2023-06-29 08:23:08 +0200

    Update main.tf

--- infra/modules/storage-blob/main.tf
+++ infra/modules/storage-blob/main.tf
@@ -1,10 +1,15 @@
+# Configure the Azure provider
 terraform {
   required_providers {
-    azurecaf = {
-      source  = "aztfmod/azurecaf"
-      version = "1.2.16"
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0.0"
     }
   }
+  required_version = ">= 0.14.9"
+}
+provider "azurerm" {
+  features {}
 }
 
 resource "azurecaf_name" "storage_account" {
